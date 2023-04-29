01af9a6c50ee4fe9c8bf79187646f897f145021d
madhusudhanv8999 <123716412+madhusudhanv8999@users.noreply.github.com>
2023-04-29 13:02:17 +0530

    Update elb.tfupdate

--- elb.tf
+++ elb.tf
@@ -29,7 +29,7 @@ resource "aws_rds_cluster" "default" {
   cluster_identifier      = "aurora-cluster-demo"
   engine                  = "aurora-mysql"
   engine_version          = "5.7.mysql_aurora.2.11.2"
-  availability_zones      = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+  availability_zones      = ["us-east-1a", "us-east-1b", "us-east-1c"]
   database_name           = "mydb"
   master_username         = "foo"
   master_password         = "Rahamshaik#444555"
